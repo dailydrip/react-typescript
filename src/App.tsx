997bad1be24522d7f96f79b4d3475416aa785990
Franzé Jr <franzejr@gmail.com>
2017-08-08 18:21:41 -0300

    Using TypeScript and one simple example

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import './App.css';
+
+type Circle = {
+  radius: number;
+};
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+const logo = require('./logo.svg');
+
+class App extends React.Component<{}, {}> {
+  render() {
+    let circle: Circle = {radius: 3};
+    let person1: Person = {name: 'Josh', age: '33'};
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        {circle.radius}
+        <h3>
+          {person1.name}
+        </h3>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
